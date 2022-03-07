0357211fc45d51e7aea6782b913d09380e3f60d7
Vladimir-Vologda <vologda2018@inbox.ru>
2022-03-07 12:20:46 +0300

    Creating forms.py and admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from users.forms import CustomUserChangeFormInAdmin, CustomUserRegistrationForm
+from users.models import CustomUserModel
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeFormInAdmin
+    add_form = CustomUserRegistrationForm
+
+    list_display = (
+        'phone', 'name', 'get_full_name', 'birthday', 'photo',
+        'registration_date', 'is_active', 'is_staff', 'is_superuser', 'slug'
+    )
+    list_display_links = (
+        'phone', 'name', 'slug'
+    )
+    list_filter = (
+        'is_staff',
+    )
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'birthday', 'photo')}),
+        ('Personal info', {'fields': ('phone', 'name', 'slug')}),
+        ('Permission', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = (
+        'phone', 'name',
+    )
+    ordering = (
+        'name',
+    )
+    filter_horizontal = (
+
+    )
+
+
+admin.site.register(CustomUserModel, CustomUserAdmin)
+admin.site.unregister(Group)
