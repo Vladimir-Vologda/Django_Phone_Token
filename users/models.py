ce1470b04289c41cb20a6003c20079e2e6a91f9d
Vladimir-Vologda <vologda2018@inbox.ru>
2022-03-14 12:00:50 +0300

    Adding fields to models.py

--- users/models.py
+++ users/models.py
@@ -8,6 +8,9 @@ from users.managers import CustomUserManager
 
 
 def photo_dir(instance, filename):
+    #   Функция, при которой сохранение файлов будет
+    #   производиться в отдельную папку для каждого пользователя,
+    #   папка создаётся по его 'name'
     return f'users/{instance.name}/{filename}'
 
 
@@ -19,14 +22,20 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     photo = models.ImageField(_('Image'), upload_to=photo_dir, default='default/users_default.jpg')
     birthday = models.DateField(_('Birthday'), default='2001-01-01')
     registration_date = models.DateField(_('Registration date'), auto_now_add=True)
+    verified_code = models.CharField(_('Verification code'), max_length=5)
     is_active = models.BooleanField(_('Status active'), default=True)
     is_staff = models.BooleanField(_('Status admin'), default=False)
     is_superuser = models.BooleanField(_('Status superuser'), default=False)
+    is_verified = models.BooleanField(_('Status verified'), default=False)
     slug = models.SlugField(_('URL-address'), unique=True, db_index=True)
 
+    #   Пользовательский менеджер
     objects = CustomUserManager()
 
+    #   Обязательное поле, по которому пользователь будет входить в систему
     USERNAME_FIELD = 'phone'
+    #   Дополнительные обязательные поля,
+    #   которые вводятся при регистрации
     REQUIRED_FIELDS = ('name',)
 
     class Meta:
@@ -43,9 +52,12 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
         return True
 
     def save(self, *args, **kwargs):
+        #   Функция автоматического создания slug,
+        #   еси оно ещё не создано
         if not self.pk:
             self.slug = slugify(self.name)
         return super(CustomUserModel, self).save(**kwargs)
 
     def get_full_name(self):
+        #   Функция представления полного имени
         return f'{self.first_name} {self.last_name}'
